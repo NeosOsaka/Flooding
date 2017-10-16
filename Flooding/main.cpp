001c7a9f541ad90e783c0009f2bc238b76bb2ed0
neos <tnk2ru@gmail.com>
2017-10-16 13:33:50 +0900

    Initial Commit

---
+++ Flooding/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Flooding
+//
+//  Created by neos on 2017/10/16.
+//  Copyright (c) 2017年 Tatsuro Seno. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
