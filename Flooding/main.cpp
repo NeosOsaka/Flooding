c1dfbefe8533c31a3d04b24d72a52d1b10435c18
neos <tnk2ru@gmail.com>
2017-10-17 18:50:31 +0900

    Test

--- Flooding/main.cpp
+++ Flooding/main.cpp
@@ -19,11 +19,12 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
 	Node node[NODENUM]; //ノード
-	list<Node> senders_now(24); //現スロットで送信を行うノード集合
-	list<Node> senders_next(24); //次スロットで送信を行うノード集合
+	list<int> senders_now; //現スロットで送信を行うノード集合
+	list<int> senders_next; //次スロットで送信を行うノード集合
 	senders_now.clear();
 	senders_next.clear();
 	
+	/********** 初期設定 **********/
 	/* 各ノードを配置 */
 	node[0].Node::setXY(0, 1);
 	node[1].Node::setXY(2, 0);
@@ -31,67 +32,50 @@ int main(int argc, const char * argv[]) {
 	node[3].Node::setXY(4, 1);
 	node[4].Node::setXY(4, 4);
 	
-	/* ノードを1つだけ送信状態に */
+	/* 送信元を1つ指定 */
 	node[4].changeState();
-	senders_now.push_back(node[4]);
+	senders_now.push_back(4);
+	
 	
 	/********** Flooding開始 **********/
-	for (int i = 1; i < 20; i++) {
+	for (int timeslot = 1; timeslot < 20; timeslot++) {
 		/* タイムスロット表示 */
-		cout << "Time Slot : " << i << endl;
+		cout << "Time Slot : " << timeslot << endl;
 		
 		/* 送信ノードが残っている場合 */
 		if (senders_now.size() != 0) {
-			for (Node sender : senders_now) {
+			for (int i : senders_now) {
 				/* 送信相手を探す */
 				for (int j = 0; j < NODENUM; j++) {
 					/* ノード間の距離計算(△x+△y) */
-					int range = abs(sender.getX() - node[j].getX()) +
-								abs(sender.getY() - node[j].getY());
+					int range = abs(node[i].getX() - node[j].getX()) +
+								abs(node[i].getY() - node[j].getY());
 					
 					/* ブロードキャスト可能範囲であれば送信 */
 					if ((range != 0) && (range <= 3) && (node[j].getState() == false)) {
-						cout << "(" << sender.getX() << "," << sender.getY() << ") -> "
+						cout << "	(" << node[i].getX() << "," << node[i].getY() << ") -> "
 						<< "(" << node[j].getX() << "," << node[j].getY() << ")" << endl;
 						
 						/* "未所持"から"所持"へ */
 						node[j].changeState();
 						
 						/* 受信ノードを次スロットの送信ノード集合に追加 */
-						senders_next.push_back(node[j]);
+						senders_next.push_back(j);
 					}
 				}
 			}
 			senders_now.clear();
 			senders_now = senders_next;
 			senders_next.clear();
-			
-			/* 送信ノードが無ければFlooding終了 */
-		} else {
+		}
+		
+		/* 送信ノードが無ければFlooding終了 */
+		else {
 			cout << endl;
 			cout << "Program finished." << endl;
 			break;
 		}
 	}
-	
-//	for (int i = 0; i < NODENUM; i++) {
-//		/* ノードが送信状態であれば送信 */
-//		if (node[i].Node::getState()) {
-//			/* 送信相手を探す */
-//			for (int j = 0; j < NODENUM; j++) {
-//				/* ノード間の距離計算(△x+△y) */
-//				int range = abs(node[i].getX() - node[j].getX()) +
-//				abs(node[i].getY() - node[j].getY());
-//				
-//				/* ブロードキャスト可能範囲であれば受け取り相手としてリストに保存 */
-//				if ((range != 0) && (range <= 3)) {
-//					//node[i].addReceiver(node[j]);
-//					cout << "(" << node[i].getX() << "," << node[i].getY() << ") -> "
-//					<< "(" << node[j].getX() << "," << node[j].getY() << ")" << endl;
-//				}
-//			}
-//		}
-//	}
 	
 	return 0;
 }
