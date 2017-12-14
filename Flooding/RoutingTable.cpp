a769a3aa756f354a23c69ff53163eaf06c1841e6
neos <tnk2ru@gmail.com>
2017-12-14 16:53:18 +0900

    RoutingTableの各エントリの初期設定は済んだ ここからRoutingTableの受け渡しについて考える

--- Flooding/RoutingTable.cpp
+++ Flooding/RoutingTable.cpp
@@ -18,33 +18,31 @@ const static int GHOST = -1; //存在しないノード番号
 
 /* RTの初期設定 */
 void RoutingTable::setUp(vector<int> z_id) {
-	/* 各エントリを要素数分だけ初期値を与える */
-	Entry iv; //initial value
-	iv.hop_num = INFINITY;
-	iv.next_hop = GHOST;
-
-	for (int i = 0; i < 3*(z_id.size())+1; i++) {
-		entry.push_back(iv);
-	}
+	/* 各エントリのポリシーを決定し、エントリをRTに追加する */
+	/* N桁合致(=自分自身)のZ_IDを追加 */
+	Entry myself;
+	myself.policy = z_id;
+	myself.hop_num = 0;
+	myself.next_hop = -1; //自分自身のNextHopどうするか?
+	entry.push_back(myself);
 	
-//	/* 各エントリのポリシーを決定し、エントリにRTに追加する */
-//	for (int i = 0; i <= z_id.size(); i++) {
-//		Entry temp;
-//		temp.hop_num = INFINITY;
-//		temp.next_hop = GHOST;
-//		vector<int> policy;
-//		
-//		for (int j = 0; j < i; j++) {
-//			policy.push_back(z_id[j]);
-//		}
-//		
-//		for (int k = 0; k < 4; k++) {
-//			if (z_id[policy.size()] != k) {
-//				policy.push_back(k);
-//				temp.policy = policy;
-//				entry.push_back(temp);
-//				policy.pop_back();
-//			}
-//		}
-//	}
+	for (int i = 0; i < z_id.size(); i++) {
+		Entry temp;
+		temp.hop_num = INFINITY;
+		temp.next_hop = GHOST;
+		vector<int> policy = z_id;
+		
+		for (int j = 0; j <= i; j++) {
+			policy.pop_back();
+		}
+		
+		for (int k = 0; k < 4; k++) {
+			if (z_id[policy.size()] != k) {
+				policy.push_back(k);
+				temp.policy = policy;
+				entry.push_back(temp);
+				policy.pop_back();
+			}
+		}
+	}
 }
